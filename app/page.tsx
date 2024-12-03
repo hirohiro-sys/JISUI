13e03719306848b00441715391642fbad04b7070
hirohiro-sys <katohiro8843@icloud.com>
2024-12-03 15:14:22 +0900

    cicdテスト

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Page() {
     <>
       <h1 className="text-red-400">Hello, Next.js!</h1>
       <p>環境変数の値：{key}</p>
+      <p>Github actionsから自動デプロイしました。</p>
     </>
   );
 }
