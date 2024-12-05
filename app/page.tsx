c719e5729c06b5c7ea8031a9e3b1c8e30acaa640
hirohiro-sys <katohiro8843@icloud.com>
2024-12-05 14:46:02 +0900

    開発環境にcommit

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Page() {
       <h1 className="text-red-400">Hello, Next.js!</h1>
       <p>環境変数の値：{key}</p>
       <p>Github actionsから自動デプロイしました。</p>
+      <p>ここは開発環境です</p>
     </>
   );
 }
