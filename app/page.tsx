4ac69809fa2472f20e52606ca2b76ddfb73b9e17
hirohiro-sys <katohiro8843@icloud.com>
2025-03-18 17:34:34 +0900

    feat: supabase authでGoogle認証ができるように

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 export default function Page() {
-  const key = process.env.SAMPLE_SECRET_KEY;
   return (
     <>
       <h1>Hello, Next.js!</h1>
       <p>Github actionsから自動デプロイしました。</p>
-      <p>環境変数の値：{key}</p>
     </>
   );
 }
